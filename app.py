2c7553baf1312454463876a3dde2297a659963d5
Deivid-U <deividu@nycstudents.net>
2025-03-05 10:23:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def csv_to_list(file_path):
 
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
-print(data)  # Output the list
+""" print(data)  # Output the list """
 
 """ temperatures = ["Label", 32, 50, 77, 104]
 
@@ -21,7 +21,21 @@ celsius = list(map(lambda x: (x - 32)*5/9, temperatures[1:]))
 
 print(celsius[1:]) """
 
+def row_average(data):
+    row_averages = {}
 
+    for row in data[1:]:
+        location = row[0]
+        sales = list(map(int, row[1:]))
+        row_averages[location] = round(sum(sales) / len(sales), 2)
 
+    return row_averages
+
+averages = row_average(data)
+print(averages)
+
+def sort_profit(data):
+    ordered_list = {}
     
+
 
